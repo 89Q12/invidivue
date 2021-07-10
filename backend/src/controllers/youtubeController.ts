2d993a0cc82b21e7d3ac41e0a64212651eebab22
Phil <you@example.com>
2021-07-10 15:57:10 +0200

    implemented youtube search api backend

--- backend/src/controllers/youtubeController.ts
+++ backend/src/controllers/youtubeController.ts
@@ -1,23 +1,33 @@
 // Controller for youtuber routes like getVideoById, getChannelById
+const { Client } = require("youtubei");
 
 const getVideoById = async (req, res): Promise<any> => {};
 
 
-const getSearch = async (req, res): Promise<any> => {
-
-    const { Client } = require("youtubei");
-
-    const youtube = new Client();
-
-    const run = async () => {
-	const videos = await youtube.search("Never gonna give you up", {
-		type: "all", // video | playlist | channel | all
-	});
-	console.log(videos.length);
-    console.log(videos);
-    };
-    run();
+const getResults = async (req, res): Promise<any> => {
+    if(req.query["q"]||req.query["search_query"]){
+        let query = "";
+        if(req.query["q"])query=req.query["q"];
+        if(req.query["search_query"])query=req.query["search_query"];
+        const youtube = new Client();
+    
+        
+        const videos = await youtube.search("Never gonna give you up", {
+            type: "all", // video | playlist | channel | all
+        });
+        return res.status(200).json({
+            message: 'OK',
+            ytvideos:videos,
+        });
+        
+        
+        
+    }
+    
+    return res.status(400).json({
+        message: 'No search query',
+    });
 };
 
 //export default { getSearch };
-export default { getVideoById,getSearch };
+export default { getVideoById,getResults };
