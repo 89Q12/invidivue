fee7eaac85987fa4bf2cae9773437df51e07b517
Your Name <you@example.com>
2021-07-10 12:31:53 +0200

    added search form

---
+++ frontend/src/views/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <form>
+      <input v-model="name" placeholder="Enter Name">
+      <button type="submit" @click="handleSubmit">Save name</button>    </form>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+    }
+  }
+};
+</script>
