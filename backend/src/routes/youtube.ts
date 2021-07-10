e9cf0acac36bace19d86d7784c2356c7b6aa3cfa
Phil <you@example.com>
2021-07-10 20:29:47 +0200

    fixed channel backend. should getchannelvideos be seperate

--- backend/src/routes/youtube.ts
+++ backend/src/routes/youtube.ts
@@ -8,6 +8,6 @@ router.get('/watch', controller.getVideoById)
 router.get('/w', controller.getVideoById)
 router.get('/results', controller.getResults)
 router.get('/r', controller.getResults)
-router.get('/channel/:vidId', controller.getChannel)
-router.get('/c/:vidId', controller.getChannel)
+router.get('/channel/:cId', controller.getChannel)
+router.get('/c/:cId', controller.getChannel)
 export { router };
