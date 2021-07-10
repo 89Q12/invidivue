0180c8fdeb79567d94b3fe6c4f36251d42e4e8a6
Max Hagenguth <max@hagenguth.net>
2021-07-10 17:48:51 +0200

    Added styling and imported components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
-    <router-link to="/register">Register</router-link>|
-    <router-link to="/login">Login</router-link>|
-    <router-link to="/s">Search</router-link>
-  </div>
-  <router-view />
+	<div class="content">
+		<Header />
+		<router-view />
+		<Footer />
+	</div>
 </template>
+<script lang="ts">
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+export default {
+	name: 'App',
 
+	components: {
+		Header,
+		Footer,
+	},
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html,
+body {
+	// overflow-x: hidden;
+	font-family: 'Montserrat', 'Arial', 'Helvetica', sans-serif;
+	min-height: 100vh;
+	padding: 0%;
+	margin: 0%;
+	background-color: #232323;
 }
 </style>
