bad437b17e446f09bc5e4580442c85f9d1805286
Max Hagenguth <max@hagenguth.net>
2021-07-10 17:49:32 +0200

    Added use statement for Oruga

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Oruga from '@oruga-ui/oruga-next';
+import '@oruga-ui/oruga-next/dist/oruga.css';
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(Oruga).use(store).use(router).mount('#app');
