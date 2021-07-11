c4cd29d4f21323a274d5ed31b7e770db9dd48e26
Max Hagenguth <max@hagenguth.net>
2021-07-11 15:13:33 +0200

    Added signin route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,12 +8,12 @@ const routes: Array<RouteRecordRaw> = [
 		component: Home,
 	},
 	{
-		path: '/about',
-		name: 'About',
+		path: '/signin',
+		name: 'Signin',
 		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
+		// this generates a separate chunk (signin.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+		component: () => import(/* webpackChunkName: "signin" */ '../views/signin.vue'),
 	},
 	{
 		path: '/search',
