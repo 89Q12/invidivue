9e50d4f02e113994da5c26a09b6bcb707b0b7ca7
Max Hagenguth <max@hagenguth.net>
2021-07-11 15:17:01 +0200

    deleted helloworld import

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    
-    <HelloWorld msg="Invidivue" />
-  </div>
+	<div class="home"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+	name: 'Home',
 });
 </script>
