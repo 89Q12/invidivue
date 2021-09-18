6a156aa5e3b8bc0b5f0852774ceaecb29499b6f7
11tuvork28 <zofi@xamh.de>
2021-09-18 10:33:29 +0200

    Added user module

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,9 @@
 import { createStore } from 'vuex';
 import { video_store_module } from './modules/video_store_module';
+import { user_store_module } from './modules/user_store_module';
 export default createStore({
 	modules: {
 		video_store_module,
+		user_store_module,
 	},
 });
