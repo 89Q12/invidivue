0a6d6ed73cedf1b2ce2dcad127a0205b9d169043
11tuvork28 <zofi@xamh.de>
2021-09-18 13:23:14 +0200

    rewrote dbUtils

--- backend/src/utils/dbUtils.ts
+++ backend/src/utils/dbUtils.ts
@@ -1,34 +1,38 @@
-import {Any, createConnection, EntitySchema, EntityTarget, FindConditions, In, Repository} from "typeorm";
-
+import { Any, createConnection, EntitySchema, EntityTarget, FindConditions, In, Repository } from 'typeorm';
 
 const connection = createConnection();
 
-async function findOne(entity:EntitySchema, filter: any) {
-    return (await get_conn(entity)).findOne(filter).catch((err: any) => {return err})
+async function findOne(entity: any, filter: any) {
+	const conn = await get_repo(entity);
+	try {
+		return conn.findOne(filter);
+	} catch (error) {
+		return error;
+	}
 }
-async function findOneDelete(entity:EntitySchema, filter: any) {
-    const conn = await get_conn(entity);
-    try {
-        const obj = await conn.findOne(filter);
-        await conn.delete(obj)
-    } catch (error) {
-        return error;
-    }
+async function findOneDelete(entity: any, filter: any) {
+	const conn = await get_repo(entity);
+	try {
+		const obj = await conn.findOne(filter);
+		await conn.delete(obj);
+	} catch (error) {
+		return error;
+	}
+}
+async function save(entity: any, updatedEntity: any) {
+	return (await get_repo(entity)).save(updatedEntity).catch((err: any) => {
+		return err;
+	});
 }
 
-
-
-
-
-
-
-async function get_conn(entity:EntitySchema): Promise<Repository<any>> {
-    return (await connection).manager.getRepository(entity)
+async function get_repo(entity: any): Promise<Repository<any>> {
+    const conn = await connection;
+    return conn.manager.getRepository(entity);
 }
 
-
-export default{
-    findOne,
-    findOneDelete,
-    get_conn,
-}
+export default {
+	findOne,
+	findOneDelete,
+	save,
+    get_repo,
+};
