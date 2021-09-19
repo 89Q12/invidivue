68a9b38211723500158f1bcd3ab42a190bebd040
11tuvork28 <zofi@xamh.de>
2021-09-19 11:45:12 +0200

    Cleaned up passport middleware

--- backend/src/middleware/passport.ts
+++ backend/src/middleware/passport.ts
@@ -15,32 +15,15 @@ const passportStrat = (passport): any => {
 	
 	passport.use(
 		new JwtStrategy.Strategy(options, async function (req,jwt_payload, done) {
-			//console.log(jwt_payload);
 			const conn = await connection;
-			const users = await conn.manager.getRepository(User);
-			const user = await users.findOne({name:jwt_payload.sub});
-			//console.log(jwt_payload.sub);
+			const users = conn.manager.getRepository(User);
+			const user = await users.findOne(jwt_payload.sub);
 			if(user){
 				return done(null, user);
 
 			}else{
 				return done(null, false);
 			}
-			/*User.findById({ _id: jwt_payload.sub }, 
-				function (err, user) {
-					console.log(err);
-					console.log(user);
-					
-					if (err) {
-						return done(err, false);
-					}
-					if (user) {
-						return done(null, user);
-					} else {
-						return done(null, false);
-					}
-				}
-			);*/
 		}),
 	);
 };
