7541ff4b66ebd1ac0d95e5efe47d59476abf5f40
11tuvork28 <zofi@xamh.de>
2021-09-19 15:52:28 +0200

    Adjusted css classes/styling for video page

--- frontend/src/views/video.vue
+++ frontend/src/views/video.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="video">
-		<div v-if="video" class="player">
+	<div class="video row videoInfo">
+		<div v-if="video" class="col-12 player">
 			<video :src="videosrc===''?video.formats[0].url:videosrc" @timeupdate="timeupdate(e)" @ended="ended(e)" @error="videoerror(e)" controls autoplay></video>
 		</div>
-		<div class="videoInfo">
+		<div class="col-sm-2">
 			<select @change="(e)=>changeformat(e.target.value)" :options="video.formats" v-model="selected">
 				<option  v-for="format in video.formats" v-bind:key="format" :value="format.itag">
 					{{
@@ -13,22 +13,29 @@
 						{{format.hasVideo?(format.hasAudio?"":" Video only"):(format.hasAudio?" Audio only":"")}}
 					</option>
 			</select>
-
-			<p>{{ video.videoDetails.title }}</p>
-			<a  @click="goToChannel" ><p>{{ video.videoDetails.author.name }}| {{ video.videoDetails.author.subscriber_count / 1000 }}K</p></a>
+		</div>
+		<div class="col-sm-10"></div>
+		<div class="col-sm-8">
+			<h4>{{ video.videoDetails.title }}</h4>
+		</div>
+		<div class="col-sm-4">
+		</div>
+		<div class="col-sm-2">
+			<router-link :to="'/channel??id='+ video.videoDetails.channelId.id"> <p>{{ video.videoDetails.author.name }}| {{ video.videoDetails.author.subscriber_count / 1000 }}K</p></router-link>
 			<p>Uploaded: {{ video.videoDetails.uploadDate }}</p>
 			<p>Views: {{ video.videoDetails.viewCount / 1000 }}K</p>
 			<p>Likes: {{ video.videoDetails.likes / 1000 }}K</p>
 			<p>dislikes: {{ video.videoDetails.dislikes / 1000 }}K</p>
+		</div>
+		<div class="col-sm-8 h-100">
 			<p style="white-space: pre-line;">{{ video.videoDetails.description }}</p>
 		</div>
-		<div v-for="vid in video.related_videos" v-bind:key="vid" class="related">
-			<a @click="watchWithId(vid.id)"
-				><img :src="vid.thumbnails[0].url" :height="vid.thumbnails[0].height" :width="vid.thumbnails[0].width" />
-				{{ vid.length_seconds / 100 }} Min</a
-			>
-			<p>{{ vid.title }}</p>
-			<p>{{ vid.author.name }}</p>
+		<div v-for="vid in video.related_videos" v-bind:key="vid" class="col-sm-2 related">
+				<img :src="vid.thumbnails[0].url" :height="vid.thumbnails[0].height" :width="vid.thumbnails[0].width" />
+				{{ vid.length_seconds / 100 }} Min
+				>
+			<router-link :to="'/video?v='+ vid.id"><p>{{ vid.title }}</p></router-link>
+			<router-link :to="'/channel??id='+ vid.author.id"> <p>{{ vid.author.name }}</p></router-link>
 			<p>Uploaded: {{ vid.published }} views: {{ vid.view_count / 1000 }}K</p>
 		</div>
 	</div>
@@ -41,29 +48,14 @@ export default {
 	setup() {
 		const store = useStore();
 		const router = useRouter();
-		if(router.currentRoute.value.query.id){
-			store.dispatch('get_current_video', router.currentRoute.value.query.id);
-		}
 		const videosrc = ref("");
-		const video = computed(() => {
-			return store.state.video_store_module.current_video;});
-		onMounted(() => {
-			console.log(localStorage.getItem('current_video'));
-			if (localStorage.getItem('current_video')) {
-				store.dispatch('get_current_video', localStorage.getItem('current_video'));
-			}
-		});
+		const video = computed(() => store.state.video_store_module.current_video);
 		
+		if(router.currentRoute.value.query.v){
+			store.dispatch('get_current_video', router.currentRoute.value.query.v);
+		}
+
 		videosrc.value="";
-		async function watchWithId(id: string) {
-			await store.dispatch('get_current_video', id);
-			localStorage.setItem('current_video', id);
-			router.push({ path: 'video', query: { id: id }});
-		}
-		async function goToChannel(){
-			await store.dispatch('get_channel', video.value.channelId);
-			router.push({path: 'channel', query: { query: video.value.channelId }});
-		}
 		
 		const changeformat= (e:any)=>{
 			video.value.formats.forEach((format:any) => {
@@ -87,9 +79,7 @@ export default {
 		return {
 			video,
 			router,
-			watchWithId,
 			store,
-			goToChannel,
 			changeformat,
 			videosrc,
 			timeupdate,
