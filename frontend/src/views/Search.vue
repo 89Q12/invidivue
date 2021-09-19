3c69bb93c131cac7cd336c9bf775e706011b733f
11tuvork28 <zofi@xamh.de>
2021-09-19 15:51:53 +0200

    refactored search to use channel/video component

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,29 +1,12 @@
 <template>
 	<div class="row mx-auto">
 		<div class="col-12 mt-4"></div>
-		<div v-for="video in results" v-bind:key="video" class="col-sm-4">
+		<div v-for="video in results" v-bind:key="video" class="col-sm-3">
 			<div v-if="video.name">
-				<div class="channelpicture">
-					<div class="d-flex justify-content-center">
-						<img class="img-thumbnail w-50" :src="video.thumbnails[0].url" />
-						
-					</div>
-					<p><SubscribeButton :cid="video.id" /></p>
-						<a @click="gotochannel(video.id)" ><p>{{ video.name }}</p></a>
-						<p>{{ video.videoCount }} videos</p>
-						<p>{{ video.subscriberCount }}Subscribers</p>
-				</div>
+				<PlaylistCard :video="video"/>
 			</div>
 			<div v-else>
-				<div id="video">
-					<div class="thumbnail">
-						<img class="thumbnail" :src="video.thumbnails[0].url" width="100%" />
-						<p id="duration">{{ video.duration / 100 }} Min</p>
-					</div>
-					<a @click="watchWithId(video.id)" ><p>{{ video.title }}</p></a>
-					<p>{{ video.channel?video.channel.name:"" }} </p>
-					<p>{{ video.uploadDate }} {{ video.viewCount / 1000 }}K</p>
-				</div>
+				<VideoCard :video="video"/>
 			</div>
 		</div>
 	</div>
@@ -33,10 +16,12 @@
 import { ref, computed } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import SubscribeButton from '../components/SubscribeButton.vue';
+import PlaylistCard from '../components/DisplayChannelCard.vue';
+import VideoCard from '../components/DisplayVideoCard.vue';
 export default {
-	components: {
-		SubscribeButton,
+	    components: {
+		PlaylistCard,
+		VideoCard,
 	},
 	setup() {
 		const store = useStore();
@@ -47,22 +32,9 @@ export default {
 			console.log(router.currentRoute.value.query.query);
 			store.dispatch('get_search_result', router.currentRoute.value.query.query);
 		}
-		
-		//console.log(JSON.stringify(results.value));
-		async function watchWithId(id: string) {
-			await store.dispatch('get_current_video', id);
-			localStorage.setItem('current_video', id);
-			router.push({ path: 'video', query: { id: id }});
-		}
-		async function gotochannel(id: string) {
-			await store.dispatch('get_channel', id);
-			localStorage.setItem('current_channel', id);
-			router.push({ path: 'channel', query: { id: id }});
-		}
+
 		return {
 			results,
-			watchWithId,
-			gotochannel,
 			router,
 			store,
 		};
