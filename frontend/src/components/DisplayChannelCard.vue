c91f9148a14aa0343d2d6a9aeeac319a6522b1e9
11tuvork28 <zofi@xamh.de>
2021-09-19 15:48:56 +0200

    Added component to display channels

---
+++ frontend/src/components/DisplayChannelCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="channelpicture">
+    <div class="d-flex justify-content-center">
+        <img class="rounded-circle w-50" :src="props.video.thumbnails[0].url" />
+    </div>
+    <p><SubscribeButton :cid="props.video.id" /></p>
+        <router-link :to="'/channel??id='+ props.video.id" ><p>{{ props.video.name }}</p></router-link>
+        <p>{{ props.video.videoCount }} videos</p>
+        <p>{{ props.video.subscriberCount }}Subscribers</p> 
+    </div>
+</template>
+<script lang="ts">
+import SubscribeButton from './SubscribeButton.vue';
+export default{
+    props:{
+        video: { default: {}}
+    },
+    components: {
+		SubscribeButton,
+	},
+    setup(props: any) {
+        return{
+            props,
+
+        }
+    },
+}
+</script>
