5419d94c2212728ea8d4e419d21aa2adb4ff0563
11tuvork28 <zofi@xamh.de>
2021-09-19 15:49:10 +0200

    Added component to display videos

---
+++ frontend/src/components/DisplayVideoCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="video">
+        <div class="thumbnail">
+            <img class="thumbnail" :src="props.video.thumbnails[0].url" width="100%" />
+            <p id="duration">{{ props.video.duration / 100 }} Min</p>
+        </div>
+        <router-link :to="'/video?v='+props.video.id"><p>{{ props.video.title }}</p></router-link>
+        <router-link :to="'/channel??id='+ props.video.channel.id" > <p>{{ props.video.channel?video.channel.name:"" }} </p></router-link>
+        <p>{{ props.video.uploadDate }} {{ props.video.viewCount / 1000 }}K</p>
+    </div>
+</template>
+<script lang="ts">
+export default{
+    
+    props:{
+        video: { default: {}}
+    },
+    setup(props: any) {
+        return{
+            props,
+        }
+    },
+}
+</script>
